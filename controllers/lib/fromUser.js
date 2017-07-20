66457839933b65b6d8bdc2056441e8ae6350fb51
Adrian Carballo <adrian@topix.com>
2017-07-20 17:22:44 -0500

    Fix bugs and add tests

--- controllers/lib/fromUser.js
+++ controllers/lib/fromUser.js
@@ -2,14 +2,19 @@ const orderBy = require('lodash/orderBy')
 
 module.exports = 
 function fromUser(entries, user, disableSort) {
-	// Sort entries
-	if (disableSort == undefined) {
-		entries = orderBy(entries, ['createdAt'], ['desc'])
+	if (!user) throw Error('Unable to read user entries')
+
+	if (Array.isArray(entries)) {
+		// Sort entries
+		if (disableSort == undefined) {
+			entries = orderBy(entries, ['createdAt'], ['desc'])
+		}
+		// Add user data to the entries to id owner
+		let {_id, first_name, timezone, picture} = user
+		return entries.map(entry => {
+			entry.user = {_id, first_name, timezone, picture}
+			return entry
+		})
 	}
-	// Add user data to the entries to id owner
-	let {_id, first_name, timezone, picture} = user
-	return entries.map(entry => {
-		entry.user = {_id, first_name, timezone, picture}
-		return entry
-	})
+	return []
 }
--- tests/index.js
+++ tests/index.js
@@ -1,6 +1,7 @@
 const test = require('tape')
 const sinon = require('sinon')
 const validateEntry = require('../controllers/lib/validateEntry')
+const fromUser = require('../controllers/lib/fromUser')
 
 test('Creating entries', t => {
 
@@ -28,6 +29,33 @@ test('Creating entries', t => {
 
 	t.test('Creating valid entry', assert => {
 		assert.doesNotThrow(() => validateEntry(new Date(), 'Hello!', 1))
+		assert.end()
+	})
+})
+
+test('Reading entries', t => {
+	const mockUser = {_id: Math.random()}
+
+	t.test('No user', assert => {
+		assert.throws(() => fromUser(), 
+			'Unable to read user entries',
+			'Throws error'
+		)
+		assert.end()
+	})
+
+	t.test('No entries', assert => {
+		let entries = fromUser(mockUser.entries, mockUser)
+		assert.equals(entries.length, 0)
+		assert.end()
+	})
+
+	t.test('With entries', assert => {
+		mockUser.entries = [{slug: 'Entry'}]
+		let entries = fromUser(mockUser.entries, mockUser)
+		assert.ok(entries[0].user)
+		assert.equals(entries.length, mockUser.entries.length)
+		assert.equals(entries[0].user._id, mockUser._id)
 		assert.end()
 	})
 })
